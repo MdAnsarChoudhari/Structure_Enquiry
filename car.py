6a5b5558588e872ae32bea4dce488509978b20eb
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2026-01-07 21:12:41 +0530

    save

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 import sys
 
 if len(sys.argv)==8:
+    sctiptname = sys.argv[0]
     name = sys.argv[1]
     model = sys.argv[2]
     price = int(sys.argv[3])
@@ -8,6 +9,17 @@ if len(sys.argv)==8:
     mileage = float(sys.argv[5])
     run_per_year = int(sys.argv[6])
     petrol_price = float(sys.argv[7])
+else:
+    scriptname= sys.argv[0]
+    name="xuv700"
+    model=2023
+    price=1700000   
+    fuel_type="petrol"
+    mileage=16.5
+    run_per_year=15000
+    petrol_price=110.0
+
+
 
     def display_car_details(name, model, price, fuel_type, mileage, run_per_year, petrol_price):
 
